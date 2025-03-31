0b8c4018a6e787164a952c2254981c1e8757aa2f
Sid Carroll <sidcarrollworks@gmail.com>
2025-03-31 12:05:46 -0700

    removing more old code

--- GradientShaderTool/src/components/Export/index.ts
+++ GradientShaderTool/src/components/Export/index.ts
@@ -1,5 +1,4 @@
 /**
  * Export components index file
  */
-export { ExportPanel } from './ExportPanel';
-export { ExportButton } from './ExportButton'; 
+export { ExportPanel } from "./ExportPanel";
--- GradientShaderTool/src/lib/modules/export/index.ts
+++ GradientShaderTool/src/lib/modules/export/index.ts
@@ -1,7 +1,5 @@
 /**
  * Export modules index file
  */
-export { HTMLExporter } from './HTMLExporter';
-export { JSExporter } from './JSExporter';
-export { ShaderExporter } from './ShaderExporter';
-export { ExportUI } from './ExportUI';
+export { HTMLExporter } from "./HTMLExporter";
+export { ExportUI } from "./ExportUI";
